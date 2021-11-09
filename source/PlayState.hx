52714ca4202bfab1aed879b88e653a7b9a55f22e
Robert Clarke <clarke.john.robert@gmail.com>
2021-11-08 19:32:37 -0500

    Beginning player controller

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,16 +4,28 @@ import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	public var player:Player;
+
 	override public function create()
 	{
 		super.create();
 		var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
 		text.screenCenter();
 		add(text);
+
+		this.player = new Player(50, 50);
+		add(this.player);
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		player.update(elapsed);
+	}
+
+	override public function destroy():Void
+	{
+		super.destroy();
+		player = null;
 	}
 }
---
+++ source/Player.hx
@@ -0,0 +1,96 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.input.actions.FlxAction.FlxActionDigital;
+import flixel.input.actions.FlxActionManager;
+import flixel.util.FlxColor;
+
+// Adapted from https://github.com/HaxeFlixel/flixel-demos/blob/master/Input/FlxAction/source/Player.hx
+class Player extends FlxSprite
+{
+	static inline var VISUAL_SIZE:Int = 32;
+	static inline var SPEED:Int = 2;
+
+	static var actions:FlxActionManager;
+
+	var up:FlxActionDigital;
+	var down:FlxActionDigital;
+	var left:FlxActionDigital;
+	var right:FlxActionDigital;
+
+	var moveX:Float = 0;
+	var moveY:Float = 0;
+
+	public function new(X:Int, Y:Int)
+	{
+		super(X, Y);
+		makeGraphic(VISUAL_SIZE, VISUAL_SIZE, FlxColor.RED);
+		addInputs();
+	}
+
+	function addInputs():Void
+	{
+		// digital actions allow for on/off directional movement
+		up = new FlxActionDigital();
+		down = new FlxActionDigital();
+		left = new FlxActionDigital();
+		right = new FlxActionDigital();
+
+		if (actions == null)
+			actions = FlxG.inputs.add(new FlxActionManager());
+		actions.addActions([up, down, left, right]);
+
+		// Add keyboard inputs
+		up.addKey(UP, PRESSED);
+		up.addKey(W, PRESSED);
+		down.addKey(DOWN, PRESSED);
+		down.addKey(S, PRESSED);
+		left.addKey(LEFT, PRESSED);
+		left.addKey(A, PRESSED);
+		right.addKey(RIGHT, PRESSED);
+		right.addKey(D, PRESSED);
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+
+		velocity.x = 0;
+		velocity.y = 0;
+
+		x += moveX * SPEED;
+		y += moveY * SPEED;
+
+		moveX = 0;
+		moveY = 0;
+
+		updateDigital();
+	}
+
+	function updateDigital():Void
+	{
+		if (down.triggered)
+		{
+			moveY = 1;
+		}
+		else if (up.triggered)
+		{
+			moveY = -1;
+		}
+		if (left.triggered)
+		{
+			moveX = -1;
+		}
+		else if (right.triggered)
+		{
+			moveX = 1;
+		}
+
+		if (moveX != 0 && moveY != 0)
+		{
+			moveX *= .707;
+			moveY *= .707;
+		}
+	}
+}
