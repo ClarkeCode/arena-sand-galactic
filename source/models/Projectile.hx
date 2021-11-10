839a222163490fb2bde30c9242b338c8e397338f
Robert Clarke <clarke.john.robert@gmail.com>
2021-11-10 02:40:53 -0500

    Elaboration on Projectile class

--- source/models/Projectile.hx
+++ source/models/Projectile.hx
@@ -7,11 +7,20 @@ import flixel.math.FlxPoint;
 
 class ProjectileAttributes {
 	public var baseDamage:Float = 1;
-	public var projectileMaxHP:Int = 1;
+	public var projectileMaxHP:Float = 1;
+	public var isTrackingLifespan:Bool = false;
+	public var projectileAge:Float = 0;
+	public var maxProjectileAge:Float = 0;
+
 	public var startingX:Float = 0;
 	public var startingY:Float = 0;
 
-	public function new(?damage:Int) {}
+	public function new(?damage:Float = 1, ?projDurability:Float = 1, ?trackLifespan:Bool = false, ?maximumLifespan = 5) {
+		baseDamage = damage;
+		projectileMaxHP = projDurability;
+		isTrackingLifespan = trackLifespan;
+		maxProjectileAge = maximumLifespan;
+	}
 
 	public function setStartPosition(X:Float, Y:Float) {
 		startingX = X;
@@ -26,12 +35,24 @@ class Projectile extends FlxSprite {
 		super(X, Y);
 		velocity = vel;
 		acceleration = acc;
+
 		attributes = attr;
 		attributes.setStartPosition(x, y);
 	}
 
 	override public function update(elapsed:Float) {
 		super.update(elapsed);
+		if (attributes.isTrackingLifespan) {
+			attributes.projectileAge += elapsed;
+			// If a projectile has exceeded its maximum lifespan, it should destroy itself
+			if (attributes.projectileAge > attributes.maxProjectileAge) {
+				this.kill();
+			}
+		}
+
+		if (!inWorldBounds()) {
+			this.kill();
+		}
 	}
 
 	public function dealDamageToObject(other:FlxObject) {
