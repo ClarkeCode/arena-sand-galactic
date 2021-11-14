bb2d231c2c5b8c549fb3800681a28e93dab61480
Evan Burgess <evan@devlift.io>
2021-11-14 13:36:47 -0500

    Camera zoom

--- source/models/Camera.hx
+++ source/models/Camera.hx
@@ -17,13 +17,13 @@ class Camera extends FlxObject {
 
 	public function new(state:PlayState) {
         super();
-		// LEVEL_MIN_X = -FlxG.stage.stageWidth;
-		// LEVEL_MAX_X = FlxG.stage.stageWidth;
-		// LEVEL_MIN_Y = -FlxG.stage.stageHeight;
-		// LEVEL_MAX_Y = FlxG.stage.stageHeight;
+		LEVEL_MIN_X = -FlxG.stage.stageWidth;
+		LEVEL_MAX_X = FlxG.stage.stageWidth;
+		LEVEL_MIN_Y = -FlxG.stage.stageHeight;
+		LEVEL_MAX_Y = FlxG.stage.stageHeight;
         
-		// FlxG.camera.setScrollBoundsRect(LEVEL_MIN_X, LEVEL_MIN_Y, LEVEL_MAX_X + Math.abs(LEVEL_MIN_X), LEVEL_MAX_Y + Math.abs(LEVEL_MIN_Y), true); 
-        // FlxG.camera.zoom = 1.3;
-		// FlxG.camera.follow(state.player, PLATFORMER, 1);
+		FlxG.camera.setScrollBoundsRect(LEVEL_MIN_X, LEVEL_MIN_Y, LEVEL_MAX_X + Math.abs(LEVEL_MIN_X), LEVEL_MAX_Y + Math.abs(LEVEL_MIN_Y), true);
+		FlxG.camera.zoom = 1.3;
+		FlxG.camera.follow(state.player, PLATFORMER, 1);
 	}
 }
